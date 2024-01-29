8b3f1d7008adb7b8c6d43787bfb12a8d7e76d3a7
codyc1515 <50791984+codyc1515@users.noreply.github.com>
2024-01-30 10:26:43 +1300

    Fixes unhandled exception when creating duplicate config entry
    Fixes #169

--- custom_components/panasonic_cc/config_flow.py
+++ custom_components/panasonic_cc/config_flow.py
@@ -37,19 +37,13 @@ class FlowHandler(config_entries.ConfigFlow):
         """Get the options flow for this handler."""
         return PanasonicOptionsFlowHandler(config_entry)
 
-    async def _create_entry(self, username, password):
+    async def _check_entry(self, username, password):
         """Register new entry."""
         # Check if ip already is registered
         for entry in self._async_current_entries():
-            if entry.data[KEY_DOMAIN] == PANASONIC_DOMAIN:
-                return self.async_abort(reason="already_configured")
-
-        return self.async_create_entry(title="", data={
-            CONF_USERNAME: username, 
-            CONF_PASSWORD: password, 
-            CONF_FORCE_OUTSIDE_SENSOR: False,
-            CONF_ENABLE_DAILY_ENERGY_SENSOR: DEFAULT_ENABLE_DAILY_ENERGY_SENSOR
-            })
+            if entry.data[CONF_USERNAME] == username:
+                return self.async_abort(reason="already_configured_account")
+        return await self._create_device(username, password)
 
     async def _create_device(self, username, password):
         """Create device."""
@@ -71,6 +65,15 @@ class FlowHandler(config_entries.ConfigFlow):
 
         return await self._create_entry(username, password)
 
+    async def _create_entry(self, username, password):
+        """Register new entry."""
+        return self.async_create_entry(title="", data={
+            CONF_USERNAME: username, 
+            CONF_PASSWORD: password, 
+            CONF_FORCE_OUTSIDE_SENSOR: False,
+            CONF_ENABLE_DAILY_ENERGY_SENSOR: DEFAULT_ENABLE_DAILY_ENERGY_SENSOR
+            })
+
     async def async_step_user(self, user_input=None):
         """User initiated config flow."""
         if user_input is None:
@@ -80,14 +83,14 @@ class FlowHandler(config_entries.ConfigFlow):
                     vol.Required(CONF_PASSWORD): str
                     })
             )
-        return await self._create_device(user_input[CONF_USERNAME], user_input[CONF_PASSWORD])
+        return await self._check_entry(user_input[CONF_USERNAME], user_input[CONF_PASSWORD])
 
     async def async_step_import(self, user_input):
         """Import a config entry."""
         username = user_input.get(CONF_USERNAME)
         if not username:
             return await self.async_step_user()
-        return await self._create_device(username, user_input[CONF_PASSWORD])
+        return await self._check_entry(username, user_input[CONF_PASSWORD])
 
 class PanasonicOptionsFlowHandler(config_entries.OptionsFlow):
     """Handle Panasonic options."""
