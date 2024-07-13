70bc88ff30b0c4f3067039f38aec7b0d286e5418
Jimmy Everling <jimmy@jimmyk.se>
2024-07-13 11:14:03 +0200

    Device properties now keep track of changes

--- custom_components/panasonic_cc/pcomfortcloud/panasonicdevice.py
+++ custom_components/panasonic_cc/pcomfortcloud/panasonicdevice.py
@@ -46,46 +46,217 @@ class PanasonicDeviceInfo:
 
 class PanasonicDevice:
     def __init__(self, id: str, json = None) -> None:
-        self.id = id
+        self.id = id        
         self.load(json)
 
-    def load(self, json):
+    def load(self, json) -> bool:
+        has_changed = False
         if not self.features:
             self.features = PanasonicDeviceFeatures(json)
+            has_changed = True
         else:
-            self.features.load(json)
+            has_changed = has_changed or self.features.load(json)
         json_parameters = None
         if (json is not None and 'parameters' in json):
             json_parameters = json['parameters']
         if not self.parameters:
             self.parameters = PanasonicDeviceParameters(json_parameters)
+            has_changed = True
         else:
-            self.parameters.load(json)
+            has_changed = has_changed or self.parameters.load(json)
+        return has_changed
 
 
 class PanasonicDeviceFeatures:
     def __init__(self, json = None) -> None:
-        self.permission = 0
-        self.summer_house = 0
-        self.iAutoX = False
-        self.nanoe = False
-        self.nanoe_stand_alone = False
-        self.auto_mode = False
-        self.heat_mode = False
-        self.fan_mode = False
-        self.dry_mode = False
-        self.cool_mode = False
-        self.eco_navi = False
-        self.powerful_mode = False
-        self.quiet_mode = False
-        self.air_swing_lr = False
-        self.auto_swing_ud = False
-        self.eco_function = 0
+        self._permission = 0
+        self._summer_house = 0
+        self._iAutoX = False
+        self._nanoe = False
+        self._nanoe_stand_alone = False
+        self._auto_mode = False
+        self._heat_mode = False
+        self._fan_mode = False
+        self._dry_mode = False
+        self._cool_mode = False
+        self._eco_navi = False
+        self._powerful_mode = False
+        self._quiet_mode = False
+        self._air_swing_lr = False
+        self._auto_swing_ud = False
+        self._eco_function = 0
+
+        self._has_changed = False
         self.load(json)
 
-    def load(self, json):
-        if not json:
+    @property
+    def has_changed(self):
+        return self._has_changed
+    
+    @property
+    def permission(self):
+        return self._permission
+    @permission.setter
+    def permission(self, value):
+        if self._permission == value:
             return
+        self._permission = value
+        self._has_changed = True
+
+    @property
+    def summer_house(self):
+        return self._summer_house
+    @summer_house.setter
+    def summer_house(self, value):
+        if self._summer_house == value:
+            return
+        self._summer_house = value
+        self._has_changed = True
+
+    @property
+    def iAutoX(self):
+        return self._iAutoX
+    @iAutoX.setter
+    def iAutoX(self, value):
+        if self._iAutoX == value:
+            return
+        self._iAutoX = value
+        self._has_changed = True
+
+    @property
+    def nanoe(self):
+        return self._nanoe
+    @nanoe.setter
+    def nanoe(self, value):
+        if self._nanoe == value:
+            return
+        self._nanoe = value
+        self._has_changed = True
+
+    @property
+    def nanoe_stand_alone(self):
+        return self._nanoe_stand_alone
+    @nanoe_stand_alone.setter
+    def nanoe_stand_alone(self, value):
+        if self._nanoe_stand_alone == value:
+            return
+        self._nanoe_stand_alone = value
+        self._has_changed = True
+
+    @property
+    def auto_mode(self):
+        return self._auto_mode
+    @auto_mode.setter
+    def auto_mode(self, value):
+        if self._auto_mode == value:
+            return
+        self._auto_mode = value
+        self._has_changed = True
+
+    @property
+    def heat_mode(self):
+        return self._heat_mode
+    @heat_mode.setter
+    def heat_mode(self, value):
+        if self._heat_mode == value:
+            return
+        self._heat_mode = value
+        self._has_changed = True
+
+    @property
+    def fan_mode(self):
+        return self._fan_mode
+    @fan_mode.setter
+    def fan_mode(self, value):
+        if self._fan_mode == value:
+            return
+        self._fan_mode = value
+        self._has_changed = True
+
+    @property
+    def dry_mode(self):
+        return self._dry_mode
+    @dry_mode.setter
+    def dry_mode(self, value):
+        if self._dry_mode == value:
+            return
+        self._dry_mode = value
+        self._has_changed = True
+
+    @property
+    def cool_mode(self):
+        return self._cool_mode
+    @cool_mode.setter
+    def cool_mode(self, value):
+        if self._cool_mode == value:
+            return
+        self._cool_mode = value
+        self._has_changed = True
+
+    @property
+    def eco_navi(self):
+        return self._eco_navi
+    @eco_navi.setter
+    def eco_navi(self, value):
+        if self._eco_navi == value:
+            return
+        self._eco_navi = value
+        self._has_changed = True
+
+    @property
+    def powerful_mode(self):
+        return self._powerful_mode
+    @powerful_mode.setter
+    def powerful_mode(self, value):
+        if self._powerful_mode == value:
+            return
+        self._powerful_mode = value
+        self._has_changed = True
+
+    @property
+    def quiet_mode(self):
+        return self._quiet_mode
+    @quiet_mode.setter
+    def quiet_mode(self, value):
+        if self._quiet_mode == value:
+            return
+        self._quiet_mode = value
+        self._has_changed = True
+
+    @property
+    def air_swing_lr(self):
+        return self._air_swing_lr
+    @air_swing_lr.setter
+    def air_swing_lr(self, value):
+        if self._air_swing_lr == value:
+            return
+        self._air_swing_lr = value
+        self._has_changed = True
+
+    @property
+    def auto_swing_ud(self):
+        return self._auto_swing_ud
+    @auto_swing_ud.setter
+    def auto_swing_ud(self, value):
+        if self._auto_swing_ud == value:
+            return
+        self._auto_swing_ud = value
+        self._has_changed = True
+
+    @property
+    def eco_function(self):
+        return self._eco_function
+    @eco_function.setter
+    def eco_function(self, value):
+        if self._eco_function == value:
+            return
+        self._eco_function = value
+        self._has_changed = True
+
+    def load(self, json) -> bool:        
+        if not json:
+            return False
+        self._has_changed = False
         if 'permission' in json:
             self.permission = json['permission']
         if 'summerHouse' in json:
@@ -118,48 +289,195 @@ class PanasonicDeviceFeatures:
             self.auto_swing_ud = json['autoSwingUD']
         if 'ecoFunction' in json:
             self.eco_function = json['ecoFunction']
+        has_changed = self._has_changed
+        self._has_changed = False
+        return has_changed
         
 class PanasonicDeviceParameters:
     def __init__(self, json = None) -> None:
-        self.power = constants.Power.Off
-        self.mode = constants.OperationMode.Auto
-        self.fan_speed = constants.FanSpeed.Auto
-        self.horizontal_swing_mode = constants.AirSwingLR.Mid
-        self.vertical_swing_mode = constants.AirSwingUD.Mid
-        self.eco_mode = constants.EcoMode.Auto
-        self.nanoe_mode = constants.NanoeMode.Unavailable
-        self.eco_navi_mode = constants.EcoNaviMode.Unavailable
-        self.eco_function_mode = constants.EcoFunctionMode.Unavailable
-        self.target_temperature: int = None
-        self.inside_temperature: int = None
-        self.outside_temperature: int = None
-        self.zones: list[PanasonicDeviceZone] = []
+        self._power = constants.Power.Off
+        self._mode = constants.OperationMode.Auto
+        self._fan_speed = constants.FanSpeed.Auto
+        self._horizontal_swing_mode = constants.AirSwingLR.Mid
+        self._vertical_swing_mode = constants.AirSwingUD.Mid
+        self._eco_mode = constants.EcoMode.Auto
+        self._nanoe_mode = constants.NanoeMode.Unavailable
+        self._eco_navi_mode = constants.EcoNaviMode.Unavailable
+        self._eco_function_mode = constants.EcoFunctionMode.Unavailable
+        self._target_temperature: int = None
+        self._inside_temperature: int = None
+        self._outside_temperature: int = None
+        self._zones: list[PanasonicDeviceZone] = []
+        self._zone_index: dict[int, PanasonicDeviceZone] = {}
+        self._has_changed = False
         self.load(json)
         
+    @property
+    def has_changed(self):
+        return self._has_changed
+    
+    @property
+    def power(self):
+        return self._power
+    @power.setter
+    def power(self, value):
+        if self._power == value:
+            return
+        self._power = value
+        self._has_changed = True
 
-    def load(self, json):
+    @property
+    def mode(self):
+        return self._mode
+    @mode.setter
+    def mode(self, value):
+        if self._mode == value:
+            return
+        self._mode = value
+        self._has_changed = True
+
+    @property
+    def fan_speed(self):
+        return self._fan_speed
+    @fan_speed.setter
+    def fan_speed(self, value):
+        if self._fan_speed == value:
+            return
+        self._fan_speed = value
+        self._has_changed = True
+
+    @property
+    def horizontal_swing_mode(self):
+        return self._horizontal_swing_mode
+    @horizontal_swing_mode.setter
+    def horizontal_swing_mode(self, value):
+        if self._horizontal_swing_mode == value:
+            return
+        self._horizontal_swing_mode = value
+        self._has_changed = True
+
+    @property
+    def vertical_swing_mode(self):
+        return self._vertical_swing_mode
+    @vertical_swing_mode.setter
+    def vertical_swing_mode(self, value):
+        if self._vertical_swing_mode == value:
+            return
+        self._vertical_swing_mode = value
+        self._has_changed = True
+
+    @property
+    def eco_mode(self):
+        return self._eco_mode
+    @eco_mode.setter
+    def eco_mode(self, value):
+        if self._eco_mode == value:
+            return
+        self._eco_mode = value
+        self._has_changed = True
+
+    @property
+    def nanoe_mode(self):
+        return self._nanoe_mode
+    @nanoe_mode.setter
+    def nanoe_mode(self, value):
+        if self._nanoe_mode == value:
+            return
+        self._nanoe_mode = value
+        self._has_changed = True
+
+    @property
+    def eco_navi_mode(self):
+        return self._eco_navi_mode
+    @eco_navi_mode.setter
+    def eco_navi_mode(self, value):
+        if self._eco_navi_mode == value:
+            return
+        self._eco_navi_mode = value
+        self._has_changed = True
+
+    @property
+    def eco_function_mode(self):
+        return self._eco_function_mode
+    @eco_function_mode.setter
+    def eco_function_mode(self, value):
+        if self._eco_function_mode == value:
+            return
+        self._eco_function_mode = value
+        self._has_changed = True
+
+    @property
+    def target_temperature(self):
+        return self._target_temperature
+    @target_temperature.setter
+    def target_temperature(self, value):
+        if self._target_temperature == value:
+            return
+        self._target_temperature = value
+        self._has_changed = True
+
+    @property
+    def inside_temperature(self):
+        return self._inside_temperature
+    @inside_temperature.setter
+    def inside_temperature(self, value):
+        if self._inside_temperature == value:
+            return
+        self._inside_temperature = value
+        self._has_changed = True
+
+    @property
+    def outside_temperature(self):
+        return self._outside_temperature
+    @outside_temperature.setter
+    def outside_temperature(self, value):
+        if self._outside_temperature == value:
+            return
+        self._outside_temperature = value
+        self._has_changed = True
+
+    @property
+    def zones(self):
+        return self._zones
+
+    def load(self, json) -> bool:
         _LOGGER.debug('Loading device paramters, has data: %s', json is not None)
         if not json:
-            return
+            return False
+        self._has_changed = False
+
         self.power = read_enum(json, 'operate', constants.Power, self.power)
         self.mode = read_enum(json, 'operationMode', constants.OperationMode, self.mode)
         self.fan_speed = read_enum(json, 'fanSpeed', constants.FanSpeed, self.fan_speed)
         
         self._load_swing_mode(json)
-        self._load_temperature(json)
-        self._load_zones(json)
+        self._load_temperature(json)        
 
         self.eco_mode = read_enum(json, 'ecoMode', constants.EcoMode, self.eco_mode)
         self.nanoe_mode = read_enum(json, 'nanoe', constants.NanoeMode, self.nanoe_mode)
         self.eco_navi_mode = read_enum(json, 'ecoNavi', constants.EcoNaviMode, self.eco_navi_mode)
         self.eco_function_mode = read_enum(json, 'ecoFunctionData', constants.EcoFunctionMode, self.eco_function_mode)
         
+        has_changed = self._has_changed or self._load_zones(json)
+        self._has_changed = False
+        return has_changed
 
-    def _load_zones(self, json):
+    def _load_zones(self, json) -> bool:
         if 'zoneParameters' not in json:
-            return
+            return False
+        has_changed = False
+
         for zone in json['zoneParameters']:
-            self.zones.append(PanasonicDeviceZone(zone))
+            if 'zoneId' not in zone:
+                continue
+            id = zone['zoneId']
+            if id in self._zone_index:
+                has_changed = has_changed or self._zone_index[id].load(zone)
+                continue
+            self._zone_index[id] = PanasonicDeviceZone(zone)
+            self._zones.append(self._zone_index[id])
+            has_changed = True
+        return has_changed
         
 
     def _load_temperature(self, json):
@@ -194,20 +512,81 @@ class PanasonicDeviceParameters:
         
 
 class PanasonicDeviceZone:
-    def __init__(self, json = None) -> None:
-        self.id:int = None
-        self.name:str = None
-        self.mode = constants.ZoneMode.Off
-        self.level = 100
-        self.spill = 0
-        self.temperature: int = None
+    def __init__(self, json) -> None:
+        if 'zoneId' not in json:
+            raise ValueError('Invalid zone json')            
+
+        self._id:int = json['zoneId']
+        self._name:str = None
+        self._mode = constants.ZoneMode.Off
+        self._level = 100
+        self._spill = 0
+        self._temperature: int = None
+        self._has_changed = False
         self.load(json)
 
-    def load(self, json):
-        if not json:
+    @property
+    def id(self):
+        return self._id
+    
+    @property
+    def has_changed(self) -> bool:
+        return self._has_changed
+    
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, value):
+        if self._name == value:
             return
-        if 'zoneId' in json:
-            self.id = json['zoneId']
+        self._name = value
+        self._has_changed = True
+
+    @property
+    def mode(self):
+        return self._mode
+    @mode.setter
+    def mode(self, value):
+        if self._mode == value:
+            return
+        self._has_changed = True
+        self._mode = value
+
+    @property
+    def level(self):
+        return self._level
+    @level.setter
+    def level(self, value):
+        if self._level == value:
+            return
+        self._has_changed = True
+        self._level = value
+
+    @property
+    def spill(self):
+        return self._spill
+    @spill.setter
+    def spill(self, value):
+        if self._spill == value:
+            return
+        self._has_changed = True
+        self._spill = value
+
+    @property
+    def temperature(self):
+        return self._temperature
+    @temperature.setter
+    def temperature(self, value):
+        if self._temperature == value:
+            return
+        self._has_changed = True
+        self._temperature = value
+
+    def load(self, json) -> bool:
+        if not json:
+            return False
+        self._has_changed = False
         if 'zoneName' in json:
             self.name = json['zoneName']
         self.mode = read_enum(json, 'zoneOnOff', constants.ZoneMode, self.mode)
@@ -219,4 +598,8 @@ class PanasonicDeviceZone:
             self.temperature = json['zoneTemperature']
             if self.temperature == -255:
                 self.temperature = None
+        has_changed = self._has_changed
+        self._has_changed = False
+        return has_changed
+
         
