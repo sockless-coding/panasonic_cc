16736dc70e31d16b10b11023d6a29f9840d269bc
Jimmy Everling <jimmy@jimmyk.se>
2024-07-24 10:58:05 +0200

    Updated buttons to use data coordinator

--- custom_components/panasonic_cc/button.py
+++ custom_components/panasonic_cc/button.py
@@ -1,45 +1,61 @@
+from typing import Callable, Awaitable, Any
+from dataclasses import dataclass
 import logging
 
-from homeassistant.components.button import ButtonEntity
+from homeassistant.core import HomeAssistant
+from homeassistant.components.button import ButtonEntity, ButtonEntityDescription
 from homeassistant.const import EntityCategory
-from . import PANASONIC_DEVICES
-from .panasonic import PanasonicApiDevice
+from .const import DOMAIN, DATA_COORDINATORS
+from .coordinator import PanasonicDeviceCoordinator
+from .base import PanasonicDataEntity
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup_entry(hass, config, async_add_entities):
+@dataclass(frozen=True, kw_only=True)
+class PanasonicButtonEntityDescription(ButtonEntityDescription):
+    """Describes a Panasonic Button entity."""
+    func: Callable[[PanasonicDeviceCoordinator], Awaitable[Any]] | None = None
+
+APP_VERSION_DESCRIPTION = PanasonicButtonEntityDescription(
+    key="update_app_version",
+    name="Fetch latest app version",
+    icon="mdi:refresh",
+    entity_category=EntityCategory.DIAGNOSTIC,
+    func = lambda coordinator: coordinator.api_client.update_app_version()
+)
+
+UPDATE_DATA_DESCRIPTION = PanasonicButtonEntityDescription(
+    key="update_data",
+    name="Fetch latest data",
+    icon="mdi:update",
+    entity_category=EntityCategory.DIAGNOSTIC,
+    func  = lambda coordinator: coordinator.async_request_refresh()
+)
+
+async def async_setup_entry(hass: HomeAssistant, config, async_add_entities):
     entities = []
-    for device in hass.data[PANASONIC_DEVICES]:
-        entities.append(UpdateAppVersionButton(device, hass))
-    async_add_entities(entities, update_before_add=True)
+    data_coordinators: list[PanasonicDeviceCoordinator] = hass.data[DOMAIN][DATA_COORDINATORS]
+    for coordinator in data_coordinators:
+        entities.append(PanasonicButtonEntity(coordinator, APP_VERSION_DESCRIPTION))
+        entities.append(PanasonicButtonEntity(coordinator, UPDATE_DATA_DESCRIPTION))
         
-
-class UpdateAppVersionButton(ButtonEntity):
-
-    def __init__(self, device: PanasonicApiDevice, hass):
-        self._hass = hass
-        self._device = device
-        self._attr_entity_category = EntityCategory.DIAGNOSTIC
+    async_add_entities(entities)
+        
+class PanasonicButtonEntity(PanasonicDataEntity, ButtonEntity):
+    """Representation of a Panasonic Button."""
     
-    @property
-    def icon(self):
-        """Return the icon."""
-        return 'mdi:update'
-    
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return "Fetch latest app version"
+    entity_description: PanasonicButtonEntityDescription
 
-    async def async_press(self):
-        await self._device.update_app_version()
-
-    @property
-    def unique_id(self):
-        """Return a unique ID."""
-        return f"{self._device.id}-update_app_version"
+    def __init__(self, coordinator: PanasonicDeviceCoordinator, description: PanasonicButtonEntityDescription) -> None:
+        self.entity_description = description
+        super().__init__(coordinator, description.key)
     
-    @property
-    def device_info(self):
-        """Return a device description for device registry."""
-        return self._device.device_info
+
+    def _async_update_attrs(self) -> None:
+        """Update the attributes of the entity."""
+
+    async def async_press(self) -> None:
+        """Press the button."""
+        if self.entity_description.func:
+            await self.entity_description.func(self.coordinator)
+
